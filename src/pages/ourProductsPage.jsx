151520e253b1812635f6a60823bf15f9147dcd58
whashishkumar <wh.ashishkumar@gmail.com>
2025-08-29 14:04:00 +0530

    fix build issue

--- src/pages/ourProductsPage.jsx
+++ src/pages/ourProductsPage.jsx
@@ -11,7 +11,7 @@ export default function OurProductsPage({ category }) {
   const dispatch = useDispatch();
   const { ourProducts } = useSelector((state) => state?.allProducts) || {};
   const { allproducts } = ourProducts || {};
-  const { seo_title, seo_description, banner_image } = allproducts;
+  const { seo_title, seo_description, banner_image } = allproducts || {};
   const baseUrl = process.env.NEXT_PUBLIC_IMAGE_URL;
   const imagePath = process.env.NEXT_PUBLIC_IMAGE_PATH;
   const imageUrl = `${baseUrl}${imagePath}/${banner_image}`;
